82d16bef0ba860c0c5177c34f933805523776c06
Mathieu Lazerges <m.lazerges@gmail.com>
2016-09-17 15:59:42 +0200

    1st Commit
    creation projet

---
+++ CAlev_Devises/CAlev_Devises/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CAlev_Devises.MvcApplication" Language="C#" %>
---
+++ CAlev_Devises/CAlev_Devises/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CAlev_Devises.Startup))]
+namespace CAlev_Devises
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
